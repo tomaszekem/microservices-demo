098a63554438ffa7dbc2ceab45ec71a513336301
tomaszekem <tomasz.machulski@pollub.edu.pl>
2021-02-24 19:23:39 +0100

    Changed encoding of entrypoint files

--- notification-service/src/main/jib/entrypoint.sh
+++ notification-service/src/main/jib/entrypoint.sh
@@ -2,3 +2,4 @@
 
 echo "Starting notification-service application"
 exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "com.tomaszekem.notificationservice.NotificationServiceApplication"  "$@"
+
--- user-service/src/main/jib/entrypoint.sh
+++ user-service/src/main/jib/entrypoint.sh
@@ -2,3 +2,4 @@
 
 echo "Starting user-service application"
 exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "com.tomaszekem.userservice.UserServiceApplication"  "$@"
+
